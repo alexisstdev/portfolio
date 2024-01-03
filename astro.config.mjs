f44e1607c8cac09c994e540eb9c37953737c8fef
GeeksforGeeks <alexissanmiguel03@gmail.com>
2024-01-02 20:56:51 -0600

    Update Astro config with Vercel options

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   plugins: [preact()],
   integrations: [preact()],
-  output: "server",
-  adapter: vercel()
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    imageService: true,
+  }),
 });
