c4df788d1b2d0b342416af5389d9378697a73c4b
Your Name <alexissanmiguel03@gmail.com>
2024-06-17 17:43:50 -0500

    Update Projects.astro grid layout for responsive design

--- src/data/ExperienceItems.ts
+++ src/data/ExperienceItems.ts
@@ -2,19 +2,49 @@ import type { ExperienceItem } from '../types/types';
 
 export const EXPERIENCE_ITEMS: ExperienceItem[] = [
   {
-    title: 'Gran Logia de Tamaulipas',
-    role: 'Freelance project',
-    time: 'September 2023',
-    description:
-      'Developed a fullstack application for "Gran Logia de Tamaulipas" to streamline the management of Masonic lodges and their members. This app is currently used by a thousand users around the state.',
-    techStack: ['React', 'NodeJS', 'Express', 'MySQL', 'JWT', 'Redux'],
-  },
-  {
     title: 'Aduasis Logistica',
     role: 'Junior developer',
     time: 'January 2023 - Present',
     description:
       'Modify existing software developed in PowerBuilder to fix errors and optimize efficiency. Add new features, modules and functionalities. Generate detailed reports to document software changes and progress. Store, retrieve, and manipulate data from Microsoft SQL Server database.',
     techStack: ['Powerbuilder', 'Microsoft SQL Server'],
+  },
+  {
+    title: 'Dos Print Manager',
+    role: 'Freelance Project',
+    time: 'February 2024',
+    description:
+      'Created a comprehensive fullstack application for "Dos Print" to optimize their printer rental service management, covering tracking, billing, inventory, and maintenance.',
+    techStack: [
+      'Typescript',
+      'Prisma',
+      'React',
+      'NodeJS',
+      'Express',
+      'MySQL',
+      'JWT',
+      'Redux',
+      'Web Sockets',
+      'Cron Jobs',
+    ],
+  },
+  {
+    title: 'Gran Logia de Tamaulipas',
+    role: 'Freelance Project',
+    time: 'September 2023',
+    description:
+      'Built a fullstack application for "Gran Logia de Tamaulipas" to enhance the administration of Masonic lodges and membership activities, now actively used by thousands across the state.',
+    techStack: [
+      'Typescript',
+      'Prisma',
+      'React',
+      'NodeJS',
+      'Express',
+      'MySQL',
+      'JWT',
+      'Redux',
+      'QR Code',
+      'Web Sockets',
+    ],
   },
 ];
